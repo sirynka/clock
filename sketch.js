c8663e107f629e09ba21ff3d7f1677d2e70840f5
Sirynka <sirynka.new@gmail.com>
2022-05-16 20:37:06 +0300

    added offset, printing, fixed hour count (24 -> 12)

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const BLACK = 0
 const WHITE = 255
 
 const arrows = []
+let OFFSET
 
 class Arrow {
     constructor(rph, len) {
@@ -10,26 +11,36 @@ class Arrow {
     }
 
     draw() {
-        const angle = Date.now() / 1000 / 180 * PI / 10 * this.rph
+        const now = Date.now() / 1000 + OFFSET
+        const angle = now / 180 * PI / 10 * this.rph
         const radius = min(width, height) * 0.5 * 0.8 * this.len
         const x = radius * cos(angle)
         const y = radius * sin(angle)
-        stroke(WHITE)
+        stroke(WHITE, 100)
         strokeWeight(16)
         line(0, 0, x, y)
+
     }
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight)
-    // arrows.push(new Arrow(60, 1))
+    arrows.push(new Arrow(60, 1))
     arrows.push(new Arrow(1, 0.75))
-    arrows.push(new Arrow(1/24, 0.5))
+    arrows.push(new Arrow(1/12, 0.5))
+    OFFSET = random(60 * 60 * 24)
+
+    setInterval(() => {
+        const now = new Date((Date.now() / 1000 + OFFSET) * 1000)
+        console.log(`${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()}`)
+    }, 1000)
 }
 
 function draw() {
     background(BLACK)
     translate(width/2, height/2)
     rotate(-HALF_PI)
+    noFill()
+    circle(0, 0, min(width, height) * 0.9)
     arrows.forEach(arrow => arrow.draw())
 }
